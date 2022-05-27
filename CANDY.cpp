aa165fa01abf3b1af62b3e2fe8a5e1c2b13c0d31
Akash Mahanty <akamhy@yahoo.com>
2022-05-27 20:44:32 +0530

    add CANDY.cpp

---
+++ CANDY.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+/*
+
+Date: 27 May 2022
+Link: https://www.spoj.com/problems/CANDY/
+
+*/
+
+int main() {
+
+    while (1) {
+
+        int packets = 0;
+
+        std::cin >> packets;
+        if (packets == -1) {
+            break;
+        }
+        // std::cout << packets << std::endl;
+        std::vector<int> packet_list;
+        int sum = 0;
+        int packet_count = 0;
+        for (int i = 0; i<packets; i++) {
+            int packet;
+            std::cin >> packet;
+            sum += packet;
+            packet_count += 1;
+            packet_list.push_back(packet);
+            // std::cout << "packet " << packet << std::endl;
+            }
+
+        if (sum % packet_count != 0) {
+            std::cout << "-1" << std::endl;
+        }
+        else {
+        
+        int moves = 0;
+        
+        int candy_per_packet = sum / packets;
+
+        for (auto i = packet_list.begin(); i != packet_list.end(); ++i) {
+            int klmn = *i;  
+            int diff = candy_per_packet - klmn;
+            if ( diff > 0 ) {
+                moves += diff;
+            }
+        }
+
+        std::cout << moves << std::endl;
+
+        }
+    }
+
+
+
+    return 0;
+}
