251cf59d26857846dfff14d3146fea44e2874651
Akash Mahanty <akamhy@yahoo.com>
2022-05-28 21:06:35 +0530

    update LASTDIG.cpp, use num theory. The last digit is cyclic

--- LASTDIG.cpp
+++ LASTDIG.cpp
@@ -6,32 +6,24 @@ Date: 28 May 2022
 Link: https://www.spoj.com/problems/LASTDIG/
 */
 
-int get_last_digit(int num) {
-    int last_digit = num % 10;
-    return last_digit;
-}
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    int cases;
-    cin >> cases;
+int main()
+{
+	int cases;
+	cin >> cases;
 
-    for (int i = 0; i < cases; i++) {
-        // a^b
-        int a, b;
-        cin >> a >> b;
-        int last_digit = 1;
+	while (cases--)
+	{
+        int a,b,last_digit;
+		cin >> a >> b;
 
-        for (int i = 0; i < b; i++) {
-            last_digit = get_last_digit(last_digit*a);
+		if (b>4) b=(b%4)+4;
 
-        }
+		last_digit=1;
 
-        cout << get_last_digit(last_digit) << "\n";
+		last_digit = (int)pow(a,b)%10;
 
-    }
-
-
-    return 0;
+		cout << last_digit << "\n";
+	}
+	return 0;
 }
